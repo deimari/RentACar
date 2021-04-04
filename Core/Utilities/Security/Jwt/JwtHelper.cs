b9fac1b237f1c1b9f331f62bf9413cb0819294eb
deimari <lgtbozdogan@gmail.com>
2021-04-04 21:44:17 +0300

    .

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,10 +1,12 @@
 ﻿using Core.Entities.Concrete;
+using Core.Extensions;
 using Core.Utilities.Security.Encryption;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 
@@ -15,6 +17,7 @@ namespace Core.Utilities.Security.Jwt
         public IConfiguration Configuration { get; }
         public TokenOptions _tokenOptions;
         public DateTime _accessTokenExpiration;
+
         public JwtHelper(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -43,6 +46,10 @@ namespace Core.Utilities.Security.Jwt
         private IEnumerable<Claim> SetClaims(User user, List<OperationClaim> operationClaims)
         {
             var claims = new List<Claim>();
+            claims.AddEmail(user.Email);
+            claims.AddName($"{user.FirstName} {user.LastName}");
+            claims.AddNameIdentifier(user.Id.ToString());
+            claims.AddRoles(operationClaims.Select(c => c.Name).ToArray());
             return claims;
         }
     }
